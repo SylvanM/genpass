d8ea54f7e87afa25f95b1b826f98d2dd5639d1f5
SylvanM <alephnulltim@gmail.com>
2020-07-09 10:35:21 -0400

    Initial Commit

---
+++ genpass/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  genpass
+//
+//  Created by Sylvan Martin on 7/9/20.
+//  Copyright © 2020 Sylvan Martin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
