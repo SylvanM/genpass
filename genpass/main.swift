63fd13b312d4a22e85fafefac4fedae9a7f43f62
SylvanM <alephnulltim@gmail.com>
2020-07-31 16:49:17 -0400

    Made input more flexible and intuitive

--- genpass/main.swift
+++ genpass/main.swift
@@ -19,86 +19,79 @@ import Foundation
 let generator = Generator(excluding: [], ofLength: 16)
 
 if let firstParam = args[safe: 1] {
-
-    if let passwordLengh = Int(firstParam) {
-        
-        // if the first argument passed is a number, then treat it as the length of the password that the user wants
-        generator.passwordLength = passwordLengh
-        
-        // now check if the user specified some illegal characters
-        
-        if let secondParam = args[safe: 2] {
-            if secondParam == "-x" || secondParam == "--except" {
-                
-                // I know this is code repetition, I'll come up with a better fix later
-                
-                if argc >= 4 {
-                    generator.forbiddenCharacters = args[3..<argc].map {
-                        guard $0.count == 1 else {
-                            print("Must give a list of single characters.")
-                            print("\($0) contains more than one character.")
-                            print("Sorry, I know you're trying your best. Here's the usage of genpass:")
-                            printUsage()
-                            exit(-1)
-                        }
-                        
-                        return Character($0)
-                    }
-                } else {
-                    print("It looks like you tried giving the \(firstParam) option, but didn't specify any characters to exclude!")
-                    print("That's okay. For future reference, you should list several characters following the \(firstParam) flag that you do not want to see appear in the password.")
-                    printUsage()
-                    exit(-1)
-                }
-            }
-        }
-        
-    } else if firstParam == "-n" || firstParam == "--number" {
+    
+    if firstParam == "-h" || firstParam == "--help" {
+        printUsage()
+        exit(0)
+    }
+    
+    if firstParam == "-n" || firstParam == "--number" {
         
         // the user has given an option indicating that they want to generate a random number
         
         var range: ClosedRange<UInt>? = nil
         var number: UInt = 0
         
-        if let lowerBoundString = args[safe: 2], let upperBoundString = args[safe: 3] {
-            if let lowerBound = UInt(lowerBoundString), let upperBound = UInt(upperBoundString) {
-                range = lowerBound...upperBound
+        // the reason I'm doing nested if-let statements is so that I can give the user more information about what went wrong,
+        // so I know exactly what condition failed. I know it looks ugly and unclean but I think it's worth the ease of use until I find a better way
+        if let lowerBoundString = args[safe: 2] {
+            
+            if let upperBoundString = args[safe: 3] {
+                
+                if let lowerBound = UInt(lowerBoundString) {
+                    
+                    if let upperBound = UInt(upperBoundString) {
+                        
+                        guard lowerBound < upperBound else {
+                            error("Sorry, the lower bound must be less than the upper bound.\n\(lowerBound) is not less than \(upperBound)")
+                        }
+                        
+                        range = lowerBound...upperBound
+                        
+                    } else {
+                        error("Sorry, \(upperBoundString) could not be converted to a \(UInt.bitWidth)-bit unsigned integer.")
+                    }
+                    
+                } else {
+                    error("Sorry, \(lowerBoundString) could not be converted to a \(UInt.bitWidth)-bit unsigned integer.")
+                }
+                
+            } else {
+                print("No upper bound specified. Ignoring lower bound.")
             }
+            
         }
 
         generator.generateNumber(range, number: &number)
         print(number)
         exit(0)
         
-    } else if firstParam == "-x" || firstParam == "--excluding" {
-        
-        // the user wants us to know that the following characters are FORBIDEN from appearing in the password they are going to generate
-        if argc >= 3 {
-            generator.forbiddenCharacters = args[2..<argc].map {
-                guard $0.count == 1 else {
-                    print("Must give a list of single characters.")
-                    print("\($0) contains more than one character.")
-                    print("Sorry, I know you're trying your best. Here's the usage of genpass:")
-                    printUsage()
-                    exit(-1)
-                }
-                
-                return Character($0)
+    }
+    
+    // where we will look for the user specifying illegal characters. This is usually the first parameter, UNLESS the user has entered a password length before it.
+    var illegalFlagExpectedIndex = 1
+    
+    // If the user is specifying a specific password length, then the first parameter should be that length
+    if let passwordLength = Int(firstParam) {
+        illegalFlagExpectedIndex += 1 // we check the NEXT argument for illegal characters, since this argument is specifying password length
+        generator.passwordLength = passwordLength
+    }
+    
+    if let nextParam = args[safe: illegalFlagExpectedIndex] {
+        if nextParam == "-x" || nextParam == "--except" {
+            
+            guard argc >= 2 + illegalFlagExpectedIndex else {
+                error("Sorry, you must specify some characters to forbid from being in the password. No characters were specified after \(nextParam) flag.")
             }
+            
+            generator.forbiddenCharacters = Array(args[1 + illegalFlagExpectedIndex])
+            
         } else {
-            print("It looks like you tried giving the \(firstParam) option, but didn't specify any characters to exclude!")
-            print("That's okay. For future reference, you should list several characters following the \(firstParam) flag that you do not want to see appear in the password.")
-            printUsage()
-            exit(-1)
+            error("Unrecognized option \(nextParam)")
         }
-    } else {
-        print("Sorry, not sure what \(firstParam) means. Here's the usage:")
-        printUsage()
-        exit(-1)
     }
     
 }
-
 
 var password: String = ""
 
